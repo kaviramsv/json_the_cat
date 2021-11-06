86ece94dc489dd66e580901edb2572feeb93aebd
kaviramsv <kavitasvenkitachalam@gmail.com>
2021-11-06 00:52:15 +0000

    add refractor

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,26 @@
-const request = require('request');
-console.log(process.argv.slice(2));
-//request('https://api.thecatapi.com/v1/images/search?breed_name=Siberian', (error, response, body) => {
+const fetchBreedDescription = function(breedName, callback) {
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${process.argv.slice(2)}`, (error, response, body) => {
-  // console.log('error:', error); // Print the error if one occurred
-  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-  //console.log(typeof body)//string
+  const request = require('request');
 
-  const data = JSON.parse(body);
-  console.log(data);
-  console.log(typeof data);
-  console.log(data[0].description);//breeds.description
-})
-  ;
-//https://docs.thecatapi.com/example-by-breed
-//https://api.thecatapi.com/v1/images/search
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+   (error, response, body) => {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+     console.log('body:', body); // Print the HTML for the Google homepage.
+    //console.log(typeof body)//string
+    const data = JSON.parse(body);
+    console.log("data",data);
+    // console.log(typeof data);
+    // console.log(data[0].description);//breeds.description
+    
+    let desc =data[0] && data[0].description;
+
+
+    if (response.statusCode === 200) {//success
+      callback(null, desc);
+    } else {
+      callback(error, null);
+    }
+  });
+};
+module.exports = { fetchBreedDescription };
